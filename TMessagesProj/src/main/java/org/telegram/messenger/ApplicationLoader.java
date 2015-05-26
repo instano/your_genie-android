39dea993973f904f975b78f047face66a41d6089
Rohit Roy <rohit7roy@gmail.com>
2015-05-26 15:07:30 +0530

    Appvirality added

--- TMessagesProj/src/main/java/org/telegram/messenger/ApplicationLoader.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/ApplicationLoader.java
@@ -27,6 +27,7 @@ import android.os.Build;
 import android.os.Handler;
 import android.os.PowerManager;
 
+import com.appvirality.android.AppviralityAPI;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
@@ -178,6 +179,7 @@ public class ApplicationLoader extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        AppviralityAPI.init(getApplicationContext());
 
         if (Build.VERSION.SDK_INT < 11) {
             java.lang.System.setProperty("java.net.preferIPv4Stack", "true");
