93df04ab95c9ad75cd421748b75bf4f7114053ef
Vedant Agarwala <vedant.kota@gmail.com>
2015-05-26 14:48:07 +0530

    fixed BUG: messages open instead of chat after login

--- TMessagesProj/src/main/java/org/telegram/ui/MessagesActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/MessagesActivity.java
@@ -17,6 +17,7 @@ import android.content.res.Configuration;
 import android.graphics.Outline;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -36,29 +37,29 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import org.telegram.android.AndroidUtilities;
+import org.telegram.android.ContactsController;
 import org.telegram.android.LocaleController;
 import org.telegram.android.MessageObject;
-import org.telegram.messenger.BuildVars;
-import org.telegram.messenger.FileLog;
-import org.telegram.messenger.TLRPC;
-import org.telegram.android.ContactsController;
 import org.telegram.android.MessagesController;
 import org.telegram.android.MessagesStorage;
 import org.telegram.android.NotificationCenter;
+import org.telegram.messenger.BuildVars;
+import org.telegram.messenger.FileLog;
 import org.telegram.messenger.R;
+import org.telegram.messenger.TLRPC;
 import org.telegram.messenger.UserConfig;
-import org.telegram.ui.Adapters.BaseFragmentAdapter;
-import org.telegram.ui.Adapters.DialogsAdapter;
-import org.telegram.ui.Adapters.DialogsSearchAdapter;
-import org.telegram.ui.AnimationCompat.ObjectAnimatorProxy;
-import org.telegram.ui.AnimationCompat.ViewProxy;
-import org.telegram.ui.Cells.UserCell;
-import org.telegram.ui.Cells.DialogCell;
 import org.telegram.ui.ActionBar.ActionBar;
 import org.telegram.ui.ActionBar.ActionBarMenu;
 import org.telegram.ui.ActionBar.ActionBarMenuItem;
 import org.telegram.ui.ActionBar.BaseFragment;
 import org.telegram.ui.ActionBar.MenuDrawable;
+import org.telegram.ui.Adapters.BaseFragmentAdapter;
+import org.telegram.ui.Adapters.DialogsAdapter;
+import org.telegram.ui.Adapters.DialogsSearchAdapter;
+import org.telegram.ui.AnimationCompat.ObjectAnimatorProxy;
+import org.telegram.ui.AnimationCompat.ViewProxy;
+import org.telegram.ui.Cells.DialogCell;
+import org.telegram.ui.Cells.UserCell;
 
 import java.util.ArrayList;
 
@@ -631,7 +632,19 @@ public class MessagesActivity extends BaseFragment implements NotificationCenter
         if (dialogsSearchAdapter != null) {
             dialogsSearchAdapter.notifyDataSetChanged();
         }
-        presentFragment(new ChatActivity(BuildVars.args), true);
+        openChatActivity();
+    }
+
+    private void openChatActivity() {
+        if (!presentFragment(new ChatActivity(BuildVars.args), true)) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if (!presentFragment(new ChatActivity(BuildVars.args), true))
+                        openChatActivity();
+                }
+            }, 50);
+        }
     }
 
     @Override
