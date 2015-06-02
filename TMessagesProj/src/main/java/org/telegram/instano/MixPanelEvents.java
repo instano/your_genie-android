187f0889fe4da3bc3dd01e41db5a6af6436550d0
Dheeraj <dheerajmeena0709@gmail.com>
2015-06-02 12:49:17 +0530

    Mix panel changes

--- TMessagesProj/src/main/java/org/telegram/instano/MixPanelEvents.java
+++ TMessagesProj/src/main/java/org/telegram/instano/MixPanelEvents.java
@@ -25,6 +25,33 @@ public class MixPanelEvents {
     public static final String USER_PROPERTY_LAST_NAME = "$last_name";
     public static final String USER_PROPERTY_PHONE = "$phone";
 
+//    LaunchActivity button click
+    public static final String LAUNCH_INVITE_FREINDS = "Invite Friends";
+    public static final String LAUNCH_SETTINGS = "Settings";
+    public static final String LAUNCH_ABOUT_US = "About Us";
+    public static final String LAUNCH_CONTACT_US = "Contact Us";
+
+//    SettingsActivity button click
+    public static final String SETTINGS_USER_PHONE_NUMBER = "Phone";
+    public static final String SETTINGS_USER_NAME = "Name";
+    public static final String SETTINGS_NOTIFICATIONS_SOUNDS = "Notification";
+    public static final String SETTINGS_SECURITY = "Security";
+    public static final String SETTINGS_CHAT_BACKGROUND = "Chat Background";
+    public static final String SETTINGS_LANGUAGE = "Language";
+    public static final String SETTINGS_ANIMATIONS = "Animations";
+    public static final String SETTINGS_USING_MOBILE_DATA = "Mobile Data";
+    public static final String SETTINGS_USING_WI_FI = "Wi-Fi";
+    public static final String SETTINGS_USING_ROAMING = "Roaming";
+    public static final String SETTINGS_SAVE_TO_GALLERY = "Save To Gallery";
+    public static final String SETTINGS_MESSAGE_TEXT_SIZE = "Text Size";
+    public static final String SETTINGS_SEND_BY_ENTER = "Send By Enter";
+
+//    ProfileActivity button click
+    public static final String PROFILE_SENDER_PHONE_NUMBER = "Sender Number";
+    public static final String PROFILE_NOTIFICATION_AND_SOUNDS = "Notification And Sounds";
+    public static final String PROFILE_SHARED_MEDIA = "Shared Media";
+
+
     public static MixpanelAPI api() {
         return MixpanelAPI.getInstance(ApplicationLoader.applicationContext, BuildVars.MIXPANEL_TOKEN);
     }
--- TMessagesProj/src/main/java/org/telegram/ui/SettingsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/SettingsActivity.java
@@ -44,10 +44,13 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.mixpanel.android.mpmetrics.MixpanelAPI;
+
 import org.telegram.android.AndroidUtilities;
 import org.telegram.android.ContactsController;
 import org.telegram.PhoneFormat.PhoneFormat;
 import org.telegram.android.MediaController;
+import org.telegram.instano.MixPanelEvents;
 import org.telegram.messenger.ApplicationLoader;
 import org.telegram.messenger.BuildVars;
 import org.telegram.android.LocaleController;
@@ -249,7 +252,7 @@ public class SettingsActivity extends BaseFragment implements NotificationCenter
     }
 
     @Override
-    public View createView(Context context, LayoutInflater inflater) {
+    public View createView(final Context context, LayoutInflater inflater) {
         actionBar.setBackgroundColor(AvatarDrawable.getProfileBackColorForId(5));
         actionBar.setItemsBackground(AvatarDrawable.getButtonColorForId(5));
         actionBar.setBackButtonImage(R.drawable.ic_ab_back);
@@ -376,6 +379,7 @@ public class SettingsActivity extends BaseFragment implements NotificationCenter
                     if (getParentActivity() == null) {
                         return;
                     }
+                    MixpanelAPI.getInstance(context,BuildVars.MIXPANEL_TOKEN).track(MixPanelEvents.SETTINGS_MESSAGE_TEXT_SIZE,null);
                     AlertDialog.Builder builder = new AlertDialog.Builder(getParentActivity());
                     builder.setTitle(LocaleController.getString("TextSize", R.string.TextSize));
                     final NumberPicker numberPicker = new NumberPicker(getParentActivity());
@@ -398,6 +402,7 @@ public class SettingsActivity extends BaseFragment implements NotificationCenter
                     });
                     showAlertDialog(builder);
                 } else if (i == enableAnimationsRow) {
+                    MixpanelAPI.getInstance(context,BuildVars.MIXPANEL_TOKEN).track(MixPanelEvents.SETTINGS_ANIMATIONS,null);
                     SharedPreferences preferences = ApplicationLoader.applicationContext.getSharedPreferences("mainconfig", Activity.MODE_PRIVATE);
                     boolean animations = preferences.getBoolean("view_animations", true);
                     SharedPreferences.Editor editor = preferences.edit();
@@ -407,10 +412,13 @@ public class SettingsActivity extends BaseFragment implements NotificationCenter
                         ((TextCheckCell) view).setChecked(!animations);
                     }
                 } else if (i == notificationRow) {
+                    MixpanelAPI.getInstance(context,BuildVars.MIXPANEL_TOKEN).track(MixPanelEvents.SETTINGS_NOTIFICATIONS_SOUNDS,null);
                     presentFragment(new NotificationsSettingsActivity());
                 } else if (i == backgroundRow) {
+                    MixpanelAPI.getInstance(context,BuildVars.MIXPANEL_TOKEN).track(MixPanelEvents.SETTINGS_CHAT_BACKGROUND,null);
                     presentFragment(new WallpapersActivity());
                 } else if (i == sendByEnterRow) {
+                    MixpanelAPI.getInstance(context,BuildVars.MIXPANEL_TOKEN).track(MixPanelEvents.SETTINGS_SEND_BY_ENTER,null);
                     SharedPreferences preferences = ApplicationLoader.applicationContext.getSharedPreferences("mainconfig", Activity.MODE_PRIVATE);
                     boolean send = preferences.getBoolean("send_by_enter", false);
                     SharedPreferences.Editor editor = preferences.edit();
@@ -420,13 +428,16 @@ public class SettingsActivity extends BaseFragment implements NotificationCenter
                         ((TextCheckCell) view).setChecked(!send);
                     }
                 } else if (i == saveToGalleryRow) {
+                    MixpanelAPI.getInstance(context,BuildVars.MIXPANEL_TOKEN).track(MixPanelEvents.SETTINGS_SAVE_TO_GALLERY,null);
                     MediaController.getInstance().toggleSaveToGallery();
                     if (view instanceof TextCheckCell) {
                         ((TextCheckCell) view).setChecked(MediaController.getInstance().canSaveToGallery());
                     }
                 } else if (i == privacyRow) {
+                    MixpanelAPI.getInstance(context,BuildVars.MIXPANEL_TOKEN).track(MixPanelEvents.SETTINGS_SECURITY,null);
                     presentFragment(new PrivacySettingsActivity());
                 } else if (i == languageRow) {
+                    MixpanelAPI.getInstance(context,BuildVars.MIXPANEL_TOKEN).track(MixPanelEvents.SETTINGS_LANGUAGE,null);
 //                    presentFragment(new LanguageSelectActivity());
                 } else if (i == contactsSortRow) {
                     if (getParentActivity() == null) {
@@ -460,12 +471,15 @@ public class SettingsActivity extends BaseFragment implements NotificationCenter
 
                     int mask = 0;
                     if (i == mobileDownloadRow) {
+                        MixpanelAPI.getInstance(context,BuildVars.MIXPANEL_TOKEN).track(MixPanelEvents.SETTINGS_USING_MOBILE_DATA,null);
                         builder.setTitle(LocaleController.getString("WhenUsingMobileData", R.string.WhenUsingMobileData));
                         mask = MediaController.getInstance().mobileDataDownloadMask;
                     } else if (i == wifiDownloadRow) {
+                        MixpanelAPI.getInstance(context,BuildVars.MIXPANEL_TOKEN).track(MixPanelEvents.SETTINGS_USING_WI_FI,null);
                         builder.setTitle(LocaleController.getString("WhenConnectedOnWiFi", R.string.WhenConnectedOnWiFi));
                         mask = MediaController.getInstance().wifiDownloadMask;
                     } else if (i == roamingDownloadRow) {
+                        MixpanelAPI.getInstance(context,BuildVars.MIXPANEL_TOKEN).track(MixPanelEvents.SETTINGS_USING_ROAMING,null);
                         builder.setTitle(LocaleController.getString("WhenRoaming", R.string.WhenRoaming));
                         mask = MediaController.getInstance().roamingDownloadMask;
                     }
@@ -522,8 +536,10 @@ public class SettingsActivity extends BaseFragment implements NotificationCenter
                     builder.setNegativeButton(LocaleController.getString("OK", R.string.OK), null);
                     showAlertDialog(builder);
                 } else if (i == usernameRow) {
+                    MixpanelAPI.getInstance(context,BuildVars.MIXPANEL_TOKEN).track(MixPanelEvents.SETTINGS_USER_NAME,null);
                     presentFragment(new ChangeNameActivity());
                 } else if (i == numberRow) {
+                    MixpanelAPI.getInstance(context,BuildVars.MIXPANEL_TOKEN).track(MixPanelEvents.SETTINGS_USER_PHONE_NUMBER,null);
                     presentFragment(new ChangePhoneHelpActivity());
                 }
             }
